174565f02a884a41db0e578196dd17cccf284e5d
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-11-26 20:28:51 -0700

    Updated dev env file.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+// Ignore localhost SSL errors
+// Try/Catch is only to stop browser errors.
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+export const environment = {
+    production: false,
+    apiEndpoint: "https://localhost:7255/api/",
+};
