6c72c8e34a119df3f2525ad2165cba299e57be26
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-11-28 11:12:48 -0700

    Added try/catch back.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 // Ignore localhost SSL errors
 // Try/Catch is only to stop browser errors.
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+try {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+} catch(err) {}
 
 export const environment = {
     production: false,
