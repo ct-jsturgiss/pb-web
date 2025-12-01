24c850a125d37cde9ab44240a9859720dfaedcb4
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-11-30 21:31:15 -0700

    Added query func for iv lookup paths.

--- src/app/services/inventory/iv-api-service.ts
+++ src/app/services/inventory/iv-api-service.ts
@@ -4,6 +4,7 @@ import { map, Observable } from "rxjs";
 import { ApiQueryRequest, ApiQueryResult } from "../api-interfaces";
 import { InventoryLookup, InventoryLookupAdapter } from "../../models/inventory/inventory-lookup";
 import { HttpClient } from "@angular/common/http";
+import { IvLookupPath, IvLookupPathAdapter } from "../../models/inventory/inventory-lookup-path";
 
 @Injectable({ providedIn: "root"})
 export class InventoryApiService extends PbApi {
@@ -18,6 +19,13 @@ export class InventoryApiService extends PbApi {
         return this.postQuery(query)
             .pipe<InventoryLookup[]>(
                 map((result:ApiQueryResult) => (result.data?.records.map(r => InventoryLookupAdapter.adapt(r)) ?? []) as InventoryLookup[])
+            );
+    }
+
+    listInventoryLookupPaths(query:ApiQueryRequest): Observable<IvLookupPath[]> {
+        return this.postQuery(query)
+            .pipe<IvLookupPath[]>(
+                map((result:ApiQueryResult) => (result.data?.records.map(r => IvLookupPathAdapter.adapt(r)) ?? []) as IvLookupPath[])
             );
     }
 }
