b5e7f2721c238e40e9650aa6c4a60572a92e2f68
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-03 09:01:30 -0700

    Added clear button and input to control whether the button is rendered or not.

--- src/app/components/inventory/iv-lookup-question/iv-lookup-question.component.ts
+++ src/app/components/inventory/iv-lookup-question/iv-lookup-question.component.ts
@@ -1,20 +1,34 @@
-import { Component, input, InputSignal, model, ModelSignal, output, OutputEmitterRef, SimpleChanges } from '@angular/core';
+import { Component, input, InputSignal, model, ModelSignal, output, OutputEmitterRef } from '@angular/core';
 
 // primeng
 import { SelectModule } from "primeng/select"
 import { IvLookupPath } from '../../../models/inventory/inventory-lookup-path';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'pb-iv-lookup-question',
-  imports: [SelectModule, FormsModule],
+  imports: [SelectModule, FormsModule, ButtonModule],
   templateUrl: './iv-lookup-question.component.html',
   styleUrl: './iv-lookup-question.component.scss',
 })
 export class IvLookupQuestionComponent {
 
+  // Inputs
   public leafTitle:InputSignal<string> = input<string>("");
   public leafSource:InputSignal<IvLookupPath[]> = input<IvLookupPath[]>([]);
+  public showClearButton:InputSignal<boolean> = input<boolean>(true);
+
+  // Outputs
+  public clearButtonClicked:OutputEmitterRef<void> = output<void>();
+
+  // Bindables
   public selectedLeaf:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+
+
+  // Handlers
+  onClearButtonClicked() {
+    this.clearButtonClicked.emit();
+  }
 
 }
