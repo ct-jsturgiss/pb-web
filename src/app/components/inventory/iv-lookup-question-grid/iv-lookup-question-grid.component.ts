c932758aa843382d2cacabfec22d470377c3c7b9
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-03 09:03:54 -0700

    Refactored question grid for new lookup state store. Added function for updating store with selection. Reorganized setup code from ctor to ngOnInit.

--- src/app/components/inventory/iv-lookup-question-grid/iv-lookup-question-grid.component.ts
+++ src/app/components/inventory/iv-lookup-question-grid/iv-lookup-question-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, model, ModelSignal, output, OutputEmitterRef } from '@angular/core';
+import { Component, input, model, ModelSignal, output, OutputEmitterRef } from '@angular/core';
 import { IvLookupQuestionComponent } from '../iv-lookup-question/iv-lookup-question.component';
 import { InventoryApiService } from '../../../services/inventory/iv-api-service';
 import { IvLookupPath } from '../../../models/inventory/inventory-lookup-path';
@@ -8,123 +8,137 @@ import { ApiQueryRequest } from '../../../services/api-interfaces';
 import { InventoryLookupAdapter } from '../../../models/inventory/inventory-lookup';
 import { InventoryConst } from '../../../../constants/ui-constants';
 import { IvLookupSelection } from '../../../models/inventory/inventory-lookup-selection';
+import { IvLookupService } from '../../../services/inventory/iv-lookup-service';
+
+// TODO: Refactor question grid. Can we simply events/bindings even more? Move state to service?
 
 @Component({
   selector: 'pb-iv-lookup-question-grid',
   imports: [IvLookupQuestionComponent],
   templateUrl: './iv-lookup-question-grid.component.html',
   styleUrl: './iv-lookup-question-grid.component.scss',
-	providers: [InventoryApiService]
 })
 export class IvLookupQuestionGridComponent {
 
-    private m_api:InventoryApiService;
-		private m_leafDict:ModelSignal<IvLookupPath[]>[] = [];
-		private m_selectedLeafDict:ModelSignal<IvLookupPath|null>[] = [];
-		private m_leafCache:IvLookupCache;
-		private m_resetting:boolean = false;
-		
-		// Leaf Sources
-		public leaf1:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf2:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf3:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf4:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf5:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf6:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
-		public leaf7:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	private m_leafDict:ModelSignal<IvLookupPath[]>[] = [];
+	private m_selectedLeafDict:ModelSignal<IvLookupPath|null>[] = [];
+	private m_leafCache:IvLookupCache = new IvLookupCache();
+	private m_resetting:boolean = false;
+	
+	// Store
+	public lookupStore = input.required<IvLookupService>();
 
-		// Leaf Selections
-		public selectedLeaf1:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf2:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf3:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf4:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf5:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf6:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
-		public selectedLeaf7:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	// Leaf Sources
+	public leaf1:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf2:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf3:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf4:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf5:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf6:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
+	public leaf7:ModelSignal<IvLookupPath[]> = model<IvLookupPath[]>([]);
 
-		// State Output
-		public leafSelection:OutputEmitterRef<IvLookupSelection> = output<IvLookupSelection>();
+	// Leaf Selections
+	public selectedLeaf1:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf2:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf3:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf4:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf5:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf6:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
+	public selectedLeaf7:ModelSignal<IvLookupPath|null> = model<IvLookupPath|null>(null);
 
-		constructor(
-			api:InventoryApiService
-		) {
-			this.m_api = api;
-			this.m_leafCache = new IvLookupCache();
+	constructor() {}
 
-			// Local Dict
-			this.m_leafDict = [
-				this.leaf1,
-				this.leaf2,
-				this.leaf3,
-				this.leaf4,
-				this.leaf5,
-				this.leaf6,
-				this.leaf7
-			];
-			this.m_selectedLeafDict = [
-				this.selectedLeaf1,
-				this.selectedLeaf2,
-				this.selectedLeaf3,
-				this.selectedLeaf4,
-				this.selectedLeaf5,
-				this.selectedLeaf6,
-				this.selectedLeaf7
-			];
+	ngOnInit() {
 
-			// Subscriptions
-			for(let i = 0; i < this.m_selectedLeafDict.length; i++) {
-				this.m_selectedLeafDict[i].subscribe(v => this.onLeafSelected.bind(this, v, i + 1)());
-			}
+		// Local Dict
+		this.m_leafDict = [
+			this.leaf1,
+			this.leaf2,
+			this.leaf3,
+			this.leaf4,
+			this.leaf5,
+			this.leaf6,
+			this.leaf7
+		];
+		this.m_selectedLeafDict = [
+			this.selectedLeaf1,
+			this.selectedLeaf2,
+			this.selectedLeaf3,
+			this.selectedLeaf4,
+			this.selectedLeaf5,
+			this.selectedLeaf6,
+			this.selectedLeaf7
+		];
 
-			// Queries
-			this.refreshLeafs();
-		}
-
-		refreshLeafs() {
-			//this.resetLeafs();
-			for(let i = InventoryConst.ivLeafs.firstLevel; i <= InventoryConst.ivLeafs.lastLevel; i++) {
-				this.m_api.listInventoryLookupPaths(this.getLeafListRequestByLevel(i))
-					.pipe(first()).subscribe(arr => {
-						this.m_leafCache.setLeafByLevel(arr, i);
-						if(i === 1) {
-							this.m_leafDict[i - 1].set(this.m_leafCache.getLeafByLevel(i));
-						}
-					});
-			}
-		}
-
-		resetLeafs(resetAt:number) {
-			if(!this.m_resetting) {
-				this.m_resetting = true;
-				for(let i = resetAt; i <= InventoryConst.ivLeafs.lastLevel; i++) {
-					this.m_selectedLeafDict[i - 1].set(null);
-				}
-				this.m_resetting = false;
-			}
-		}
-
-		// Leaf Handlers
-
-		onLeafSelected(leaf:IvLookupPath|null, level:number) {
-			if(!this.m_resetting) {
-				this.resetLeafs(level + 1);
-				const cache = this.m_leafCache.getLeafByLevel(level + 1);
-				const optSet = this.m_leafDict[level];
-				if(leaf) {
-					const filteredItems = cache.filter(l => l.parentId === leaf.pathId);
-					optSet.set(filteredItems);
-				} else {
-					optSet.set([]);
-				}
-				this.leafSelection.emit(new IvLookupSelection(this.m_selectedLeafDict.map(l => l())));
-			}
+		// Subscriptions
+		for(let i = 0; i < this.m_selectedLeafDict.length; i++) {
+			this.m_selectedLeafDict[i].subscribe(v => this.onLeafSelected.bind(this, v, i + 1)());
 		}
 
 		// Queries
+		this.refreshLeafs();
+	}
 
-		getLeafListRequestByLevel(level:number) {
-			return new ApiQueryRequest()
-				.setUri(`inventory/lookupleaf`)
-				.setRequestData({"path_level": level});
+	refreshLeafs() {
+		//this.resetLeafs();
+		for(let i = InventoryConst.ivLeafs.firstLevel; i <= InventoryConst.ivLeafs.lastLevel; i++) {
+			this.lookupStore().api.listInventoryLookupPaths(this.getLeafListRequestByLevel(i))
+				.pipe(first()).subscribe(arr => {
+					this.m_leafCache.setLeafByLevel(arr, i);
+					if(i === 1) {
+						this.m_leafDict[i - 1].set(this.m_leafCache.getLeafByLevel(i));
+					}
+				});
 		}
+	}
+
+	resetLeafs(resetAt:number) {
+		if(!this.m_resetting) {
+			this.m_resetting = true;
+			for(let i = resetAt; i <= InventoryConst.ivLeafs.lastLevel; i++) {
+				this.m_selectedLeafDict[i - 1].set(null);
+			}
+			this.m_resetting = false;
+		}
+	}
+
+	notifyLeafSelection() {
+		const newSelection = new IvLookupSelection(this.m_selectedLeafDict.map(l => l()));
+		this.lookupStore().setLeafSelection(newSelection);
+	}
+
+	// Leaf Handlers
+
+	onLeafSelected(leaf:IvLookupPath|null, level:number) {
+		if(!this.m_resetting) {
+			this.resetLeafs(level + 1);
+			const cache = this.m_leafCache.getLeafByLevel(level + 1);
+			const optSet = this.m_leafDict[level];
+			if(leaf) {
+				const filteredItems = cache.filter(l => l.parentId === leaf.pathId);
+				optSet.set(filteredItems);
+			} else {
+				optSet.set([]);
+			}
+			this.notifyLeafSelection();
+		}
+	}
+
+	onClearLeaf(ivlookupLeaf:IvLookupPath|null) {
+		const level = ivlookupLeaf?.level;
+		if(level) {
+			this.resetLeafs(level);
+			this.notifyLeafSelection();
+		} else {
+			console.warn("No leaf level was provided to clear from.");
+		}
+	}
+
+	// Queries
+
+	getLeafListRequestByLevel(level:number) {
+		return new ApiQueryRequest()
+			.setUri(`inventory/lookupleaf`)
+			.setRequestData({"path_level": level});
+	}
 }
