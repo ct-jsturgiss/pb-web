582809a3bc54660d6adc519d7a1595503709aff9
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-03 09:04:52 -0700

    Refactored lookup page and control for new state store service implementation.

--- src/app/components/app-menu/pages/iv-lookup/iv-lookup-page/iv-lookup-page.component.ts
+++ src/app/components/app-menu/pages/iv-lookup/iv-lookup-page/iv-lookup-page.component.ts
@@ -1,19 +1,27 @@
-import { Component, model, ModelSignal } from '@angular/core';
+import { Component, model, signal, SimpleChanges } from '@angular/core';
 
 // primeng
 
 // pb
 import { IvLookupComponent } from "../../../../inventory/iv-lookup/iv-lookup.component";
 import { SearchBarComponent } from "../../../../core/controls/search-bar/search-bar.component";
+import { IvLookupService } from '../../../../../services/inventory/iv-lookup-service';
+import { IvLookupState } from '../../../../../models/inventory/inventory-lookup-state';
+import { LookupMode } from '../../../../../models/core/lookup-mode';
+import { SearchBarStateStore } from '../../../../core/controls/search-bar/services/search-bar-store';
 
 @Component({
   selector: 'pb-iv-lookup-page',
-  imports: [IvLookupComponent, SearchBarComponent],
+  imports: [IvLookupComponent],
   templateUrl: './iv-lookup-page.component.html',
   styleUrl: './iv-lookup-page.component.scss',
+  providers: [IvLookupService, SearchBarStateStore]
 })
 export class IvLookupPageComponent {
 
-    public searchPattern:ModelSignal<string> = model<string>("");
- 
+    public service:IvLookupService;
+
+    constructor(service:IvLookupService) {
+      this.service = service;
+    }
 }
--- src/app/components/inventory/iv-lookup/iv-lookup.component.ts
+++ src/app/components/inventory/iv-lookup/iv-lookup.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal, model, ModelSignal, SimpleChanges } from '@angular/core';
+import { Component, input, model, ModelSignal, signal } from '@angular/core';
 
 // primeng
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,117 +7,66 @@ import { SkeletonModule } from "primeng/skeleton"
 import { ProgressSpinnerModule } from "primeng/progressspinner"
 
 // pb
-import { ApiQueryRequest } from '../../../services/api-interfaces';
-import { BehaviorSubject, first, map, Observable, of } from 'rxjs';
-import { InventoryApiService } from '../../../services/inventory/iv-api-service';
-import { InventoryLookup, InventoryLookupAdapter } from '../../../models/inventory/inventory-lookup';
-import { AsyncPipe } from '@angular/common';
-import { containsAsString } from '../../../services/core/helpers';
+import { InventoryLookup } from '../../../models/inventory/inventory-lookup';
 import { FormsModule } from '@angular/forms';
-import { LookupMode } from '../../../models/core/lookup-mode';
 import { IvLookupQuestionGridComponent } from '../iv-lookup-question-grid/iv-lookup-question-grid.component';
 import { IvLookupSelection } from '../../../models/inventory/inventory-lookup-selection';
+import { IvLookupService } from '../../../services/inventory/iv-lookup-service';
+import { AsyncPipe } from '@angular/common';
+import { SearchBarComponent } from '../../core/controls/search-bar/search-bar.component';
+import { SearchBarStateStore } from '../../core/controls/search-bar/services/search-bar-store';
+import { LookupMode } from '../../../models/core/lookup-mode';
 
 @Component({
 	selector: 'pb-iv-lookup',
-	imports: [InputTextModule, TableModule, SkeletonModule, AsyncPipe, 
-		FormsModule, ProgressSpinnerModule, IvLookupQuestionGridComponent],
+	imports: [InputTextModule, TableModule, SkeletonModule, AsyncPipe,
+		FormsModule, ProgressSpinnerModule, IvLookupQuestionGridComponent, SearchBarComponent],
 	templateUrl: './iv-lookup.component.html',
 	styleUrl: './iv-lookup.component.scss',
-	providers: [InventoryApiService],
 })
 export class IvLookupComponent {
 
-	private m_api:InventoryApiService;
+	// Services
+	public store = input.required<IvLookupService>();
 
+	// Props
+	public get searchStore():SearchBarStateStore { return this.store().searchStore; }
+
+	// Bindables
 	public isQuerying:ModelSignal<boolean> = model<boolean>(false);
 	public isLoading:ModelSignal<boolean> = model<boolean>(false);
-	public lookupStore:BehaviorSubject<InventoryLookup[]> = new BehaviorSubject<InventoryLookup[]>([]);
-	public lookupView:BehaviorSubject<InventoryLookup[]> = new BehaviorSubject<InventoryLookup[]>([]);
-	public selected:BehaviorSubject<InventoryLookup> = new BehaviorSubject<InventoryLookup>(new InventoryLookup(-1,"","", ""));
-	public searchPattern = input<string>("");
-	public lookupMode:ModelSignal<LookupMode> = model<LookupMode>(LookupMode.SearchPattern);
-	public leafSelection:BehaviorSubject<IvLookupSelection> = new BehaviorSubject<IvLookupSelection>(new IvLookupSelection([]));
+	public get lookupView() { return this.store().lookupView$; }
+	public get selectedLookup() { return this.store().selected$; }
 
-	constructor(api:InventoryApiService) {
-		this.m_api = api;
-
-		// Subscriptions
-		this.selected.subscribe(this.onSelectedChanged);
+	constructor() {
 	}
 
 	ngOnInit() {
+		console.log("Init");
+		this.searchStore.searchText$.subscribe(v => this.onSearchPatternChanged(v));
+		this.store().selected$.subscribe(v => this.onSelectedChanged(v));
+		this.store().leafSelection$.subscribe(v => this.onLeafSelectionChanged(v));
+
 		this.isQuerying.set(true);
-		const sub = this.m_api.listInventoryLookups(this.getLookupsRequest());
-		sub.pipe(first()).subscribe(async v => {
-			this.lookupStore.next(v);
-			await this.filterItems(v);
+		this.store().refreshLookups(() => {
 			this.isQuerying.set(false);
+			console.log("Initial refresh done");
 		});
-	}
-
-	ngOnChanges(changes: SimpleChanges) {
-		if(changes["searchPattern"]) {
-			if(this.lookupMode() !== LookupMode.SearchPattern) {
-				this.lookupMode.set(LookupMode.SearchPattern);
-			}
-			this.filterItems(this.lookupStore.getValue());
-		}
-	}
-
-	// Functions
-
-	async filterItems(items:InventoryLookup[]) {
-		this.isLoading.set(true);
-		if(this.lookupMode() === LookupMode.SearchPattern) {
-			const searchStr = this.searchPattern() ?? "";
-			if(!searchStr) {
-				this.lookupView.next(items);
-			} else {
-				let filteredItems:InventoryLookup[] = items;
-				await new Promise<void>((resolve) => {
-					filteredItems = filteredItems.filter(v => {
-						let ret = false;
-						if(containsAsString(v.itemCode, searchStr)) {
-							ret = true;
-						}
-						if(containsAsString(v.itemName, searchStr)) {
-							ret = true;
-						}
-
-						return ret;
-					});
-					resolve();
-				});
-
-				this.lookupView.next(filteredItems);
-			}
-		} else {
-			const targetPath:string = this.leafSelection.getValue().selectionPath;
-			let items = this.lookupStore.getValue();
-			items = items.filter(l => (l.pathId ?? "").startsWith(targetPath));
-			this.lookupView.next(items);
-		}
-		this.isLoading.set(false);
-	}
-
-	// Queries
-
-	getLookupsRequest():ApiQueryRequest {
-		return new ApiQueryRequest()
-			.setUri(`inventory/items`);
 	}
 
 	// Handlers
 
-	onSelectedChanged(item:InventoryLookup) {
-		console.log(`New:`, item);
+	onSearchPatternChanged(value:string) {
+		this.store().setMode(LookupMode.SearchPattern);
+		this.store().filterItems();
 	}
 
-	onLeafSelectionChanged(selection:IvLookupSelection) {
-		this.lookupMode.set(LookupMode.LeafSearch);
-		this.leafSelection.next(selection);
-		this.filterItems([])
-		console.log(`Selection:`, selection);
+	onSelectedChanged(item:InventoryLookup|null) {
+		console.log(`Selected:`, item);
+	}
+
+	onLeafSelectionChanged(selection:IvLookupSelection|null) {
+		this.store().setMode(LookupMode.LeafSearch);
+		this.store().filterItems();
 	}
 }
