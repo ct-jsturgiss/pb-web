ec00285fb4ba2f81f9dcd938730b42acf6815a9a
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-03 13:26:33 -0700

    Added additional properties for item details.

--- src/app/models/inventory/inventory-lookup.ts
+++ src/app/models/inventory/inventory-lookup.ts
@@ -2,6 +2,11 @@ import { ModelAdapter } from "../core/modelAdapter";
 
 export class InventoryLookup {
 
+    public supplierItemCode:string|null = null;
+    public manufacturerName:string|null = null;
+    public manufacturerDescription:string|null = null;
+    public listName:string|null = null;
+
     constructor(
         public id:number,
         public itemCode:string,
@@ -14,12 +19,19 @@ export class InventoryLookup {
 export const InventoryLookupAdapter:ModelAdapter<InventoryLookup> = {
 
     adapt(item: any): InventoryLookup {
-        return new InventoryLookup(
+        let ivl = new InventoryLookup(
             item["id"],
             item["item_code"],
             item["item_name"],
             item["path_id"]
         );
+
+        ivl.supplierItemCode = item["supplier_item_code"];
+        ivl.manufacturerName = item["manufacturer_name"];
+        ivl.manufacturerDescription = item["manufacturer_description"];
+        ivl.listName = item["list_name"];
+
+        return ivl;
     }
 
 }
