0b8bdf16134a9f72f88e0ea687c3d811c108516f
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-04 12:43:44 -0700

    Reorganize main content component to pages folder for main menu.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainContentComponent } from './components/main-content/main-content.component';
+import { MainContentComponent } from './components/app-menu/pages/main-content/main-content.component';
 import { NotFoundComponent } from './components/status/not-found/not-found.component';
 import { MainMenuKeys } from '../constants/ui-constants';
 import { IvLookupPageComponent } from './components/app-menu/pages/iv-lookup/iv-lookup-page/iv-lookup-page.component';
