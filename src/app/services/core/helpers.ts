3ad841e4801a9a7c4a3458f5457c73bfae4dca4d
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-08 13:29:29 -0700

    Added const func for comparing two string values.

--- src/app/services/core/helpers.ts
+++ src/app/services/core/helpers.ts
@@ -5,3 +5,7 @@ export const compareAsString = (x:any, y: any):boolean => {
 export const containsAsString = (x: any, y: any):boolean => {
     return String(x).toLocaleLowerCase().trim().includes(String(y).toLocaleLowerCase().trim());
 }
+
+export const asStringEqual = (x:any, y:any):boolean => {
+    return String(x).toLocaleLowerCase().trim() === String(y).toLocaleLowerCase().trim();
+}
