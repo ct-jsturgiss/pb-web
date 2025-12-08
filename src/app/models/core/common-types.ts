ad3977930241053601b227553f8eb120a283b7ad
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-08 08:35:58 -0700

    Added common types module.

---
+++ src/app/models/core/common-types.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Shorthand type for an empty lambda with no arguments.
+ */
+export type EmptyLambdaType = () => void;
+
+/**
+ * Constant func for a do-nothing empty lambda.
+ */
+export const EmptyLambda = () => {};
