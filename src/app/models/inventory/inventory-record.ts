40cd9b0f6b5c504a5b1511929d73fff42220edd1
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-13 15:44:17 -0700

    Added iv record interface and class.

---
+++ src/app/models/inventory/inventory-record.ts
@@ -0,0 +1,27 @@
+import { InventoryLookup } from "./inventory-lookup";
+
+export interface InventoryRecord extends InventoryLookup {
+    id:number;
+    itemCode:string;
+    itemName:string;
+    pathId:string|null;
+    supplierItemCode:string|null;
+    manufacturerName:string|null;
+    manufacturerDescription:string|null;
+    listName:string|null;
+}
+
+export class IvRecord implements InventoryRecord {
+
+    public pathId:string|null = null;
+    public supplierItemCode:string|null = null;
+    public manufacturerName:string|null = null;
+    public manufacturerDescription:string|null = null;
+    public listName:string|null = null;
+
+    constructor(public id:number, 
+        public itemCode:string,
+        public itemName:string) {
+    }
+
+}
