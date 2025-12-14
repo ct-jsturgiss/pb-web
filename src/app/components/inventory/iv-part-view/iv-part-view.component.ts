73cb3a3babe24631db46a26b10d93c4fd6eff176
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-13 23:13:15 -0700

    Built out prototype for part view.

--- src/app/components/inventory/iv-part-view/iv-part-view.component.ts
+++ src/app/components/inventory/iv-part-view/iv-part-view.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { IvRecord } from '../../../models/inventory/inventory-record';
+
+// primeng
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Button } from "primeng/button";
+import { TabsModule } from "primeng/tabs"
+import { InventoryLookup } from '../../../models/inventory/inventory-lookup';
 
 @Component({
   selector: 'pb-iv-part-view',
-  imports: [],
+  imports: [Button, TabsModule],
   templateUrl: './iv-part-view.component.html',
   styleUrl: './iv-part-view.component.scss',
 })
-export class IvPartViewComponent {
+export class IvPartViewComponent implements OnInit {
+
+  private m_dialogRef:DynamicDialogRef|null;
 
   // Inputs
-  public viewData:IvRecord|null = null;
+  public dialogData = input<any>();
 
+  // Models
+  public record:IvRecord|null = null;
+
+  public constructor(dialogRef:DynamicDialogRef|null = null) {
+    this.m_dialogRef = dialogRef;
+  }
+
+  ngOnInit(): void {
+    //this.record = new IvRecord();
+  }
+
+  closeDialog() {
+    if(this.m_dialogRef) {
+      this.m_dialogRef.close();
+    } else {
+      throw new Error("This view was not created as a dialog.");
+    }
+  }
 }
