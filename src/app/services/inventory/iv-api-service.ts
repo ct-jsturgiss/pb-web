7d4f48621d94b36fdd9e3bfebd384e6881f458dc
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-15 16:16:24 -0700

    Added query func for listing inventory units.

--- src/app/services/inventory/iv-api-service.ts
+++ src/app/services/inventory/iv-api-service.ts
@@ -7,12 +7,32 @@ import { HttpClient } from "@angular/common/http";
 import { IvLookupPath, IvLookupPathAdapter } from "../../models/inventory/inventory-lookup-path";
 import { response } from "express";
 import { GlobalStateStore } from "../core/global-state-store";
+import { InventoryUnit, InventoryUnitAdapter } from "../../models/inventory/inventory-unit";
 
 @Injectable({ providedIn: "root"})
 export class InventoryApiService extends PbApi {
 
     constructor(globalStateStore:GlobalStateStore, http:HttpClient) {
         super(globalStateStore, http);
+    }
+
+    listInventoryUnits(query:ApiQueryRequest): Observable<QueryData<InventoryUnit>> {
+        return this.postQuery(query)
+            .pipe<QueryData<InventoryUnit>>(
+                map((result:ApiQueryResult) => {
+                    if(result.isFatal) {
+                        return {
+                            response: result,
+                            records: []
+                        };
+                    } else {
+                        return {
+                            response: result,
+                            records: (result.data?.records.map(r => InventoryUnitAdapter.adapt(r)) ?? [])
+                        }
+                    }
+                })
+            );
     }
 
     listInventoryLookups(query:ApiQueryRequest): Observable<QueryData<InventoryLookup>> {
@@ -27,7 +47,7 @@ export class InventoryApiService extends PbApi {
                     } else {
                         return {
                             response: result,
-                            records: (result.data?.records.map(r => InventoryLookupAdapter.adapt(r)) ?? []) as InventoryLookup[]
+                            records: (result.data?.records.map(r => InventoryLookupAdapter.adapt(r)) ?? [])
                         };
                     }
                 })
