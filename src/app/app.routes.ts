13344cf5ae285b30ad5e0581d2c9e0ce2695ab7b
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-17 14:33:54 -0700

    Moved status page to pages dir.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './components/pages/main-content/main-content.component';
-import { NotFoundComponent } from './components/status/not-found/not-found.component';
+import { NotFoundComponent } from './components/pages/status/not-found/not-found.component';
 import { MainMenuKeys } from '../constants/ui-constants';
 import { IvLookupPageComponent } from './components/pages/iv-lookup/iv-lookup-page/iv-lookup-page.component';
 import { ConfigManageComponent } from './components/pages/config-manage/config-manage.component';
