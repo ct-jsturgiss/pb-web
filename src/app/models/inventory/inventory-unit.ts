0c6793c349066feb2f3e8adb086b1c1bae4e62dc
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-18 17:08:22 -0700

    Added interface for iv unit changes.

--- src/app/models/inventory/inventory-unit.ts
+++ src/app/models/inventory/inventory-unit.ts
@@ -1,4 +1,5 @@
 import { DbConst } from "../../../constants/db-constants";
+import { RecordChangeKind } from "../core/common-types";
 import { ModelAdapter } from "../core/modelAdapter";
 import { RecordWithId } from "../record-with-id";
 
@@ -6,6 +7,11 @@ export interface InventoryUnit extends RecordWithId {
     unit:string;
     unitName:string;
     pluralName:string|null;
+}
+
+export interface InventoryUnitChange {
+    record:InventoryUnit;
+    changeKind:RecordChangeKind;
 }
 
 export class InventoryUnit extends RecordWithId implements InventoryUnit {
