f0d0cd17bea5a9ca976da25e26151453a2add879
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-18 22:05:45 -0700

    Added spinner to app root.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from "primeng/button"
 import { MainInterfaceComponent } from "./components/core/main-interface/main-interface.component";
-import { MainContentComponent } from "./components/pages/main-content/main-content.component";
 import { MessageService } from 'primeng/api';
 import { ToastModule } from "primeng/toast";
 import { AppDialogComponent } from './components/core/dialogs/app-dialog/app-dialog.component';
+import { AppSpinnerComponent } from "./components/core/controls/app-spinner/app-spinner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, MainInterfaceComponent, ToastModule, AppDialogComponent],
+  imports: [RouterOutlet, ButtonModule, MainInterfaceComponent, ToastModule, AppDialogComponent, AppSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [
