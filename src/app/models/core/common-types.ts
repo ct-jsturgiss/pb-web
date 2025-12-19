35758287b8f6df69008610aa2fa30eafde1a5e7f
Jesse Sturgiss <jsturgiss@caretakerinc.com>
2025-12-18 17:07:25 -0700

    Added comment and RecordChangeKind enum.

--- src/app/models/core/common-types.ts
+++ src/app/models/core/common-types.ts
@@ -8,3 +8,13 @@ export type EmptyLambdaType = () => void;
  * Constant func for a do-nothing empty lambda.
  */
 export const EmptyLambda = () => {};
+
+/**
+ * Represents the type of record changes to be submitted to the API.
+ */
+export enum RecordChangeKind {
+    None = 0,
+    Insert = 1,
+    Update = 2,
+    Delete = 3
+}
